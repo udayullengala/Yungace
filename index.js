65f81044378af35d56b578b3e78cd7df4f100b6d
udayullengala <u.ullengala@gmail.com>
2024-01-21 16:57:07 +0530

    Added section and desgined them

---
+++ index.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay:true,
+        center:true,
+        autoplayTimeout:1500,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+});
